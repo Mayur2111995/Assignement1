4b7aa7571d92de6f241ed9ed934ecaeab29d9c0c
Mayur2111995 <barimayur1265@gmail.com>
2022-04-13 00:45:15 +0530

    Added:string

---
+++ src/CompareString.java
@@ -0,0 +1,13 @@
+public class CompareString {
+    public static void main(String[] args){
+
+            String s1 = "Mayur";
+            String s2 = "Ashu";
+
+            if(s1 == s2) {
+                System.out.println("Equal");
+            } else
+                System.out.println("Not Equal");
+        }
+    }
+
